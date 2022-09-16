980e94772547905709902bd88b46bfdfdc664256
nickhu <314484063@qq.com>
2022-09-17 02:53:28 +0800

    Revert "feature: support text tag"
    This reverts commit 1b4d83c324448f0c81ac552aaf59b7f232f0f552.

--- core/flyout_base.js
+++ core/flyout_base.js
@@ -565,9 +565,9 @@ Blockly.Flyout.prototype.show = function(xmlList) {
             this.targetWorkspace_, xml);
         contents.push({type: 'button', button: curButton});
         gaps.push(default_gap);
-      } else if (tagName == 'BUTTON' || tagName == 'TEXT') {
+      } else if (tagName == 'BUTTON' || tagName == 'LABEL') {
         // Labels behave the same as buttons, but are styled differently.
-        var isLabel = tagName == 'TEXT';
+        var isLabel = tagName == 'LABEL';
         var curButton = new Blockly.FlyoutButton(this.workspace_,
             this.targetWorkspace_, xml, isLabel);
         contents.push({type: 'button', button: curButton});
